44dc1423f4888a702e00d5a4b6836734c5c7ba05
cghil <cghilarducci@gmail.com>
2015-08-14 11:14:45 -0700

    card game challenge done

---
+++ public/javascript/southpark.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+	selectLineUp();
+
+	$('button').click(function(){
+		resetButton();
+		selectLineUp();
+	});
+});
+
+var characters = ["stan", "kyle", "kenny", "cartman", "wendy", "ike", "tweek"];
+
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+function selectLineUp(){
+	$('.tile').click(function(){
+		$(this).remove();
+		$(".list-of-suspects").append(this);
+	});
+}
+
+function resetButton(){
+	$('.tile').remove();
+	var cardTemplate = $('#tile-target').html();
+	characters.forEach(function(character){
+		var template = _.template(cardTemplate);
+		template = template({name: character, capitalizedName: capitalizeFirstLetter(character)});
+		$('#characters').append(template)
+	});
+}
