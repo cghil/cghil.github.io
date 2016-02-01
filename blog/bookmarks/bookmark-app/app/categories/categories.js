56a424b156713a77a786fd8df56bf0c2a8d2627f
cghil <cghilarducci@gmail.com>
2016-02-01 10:56:49 -0800

    - created models for bookmarks and categories - categories has function in the model - bookmarks doesn't have functions yet

--- blog/bookmarks/bookmark-app/app/categories/categories.js
+++ blog/bookmarks/bookmark-app/app/categories/categories.js
@@ -3,14 +3,23 @@ angular.module('categories', [
 ])
 
 .config(['$stateProvider', function($stateProvider) {
-	$stateProvider
-		.state('app.categories', {
-			url: '/',
-			views: {
-				'categories@': {
-					controller: 'CategoriesListCtrl as CategoriesListCtrl',
-					templateUrl: 'app/categories/categories.html'
-				}
-			}
-		})
+    $stateProvider
+        .state('app.categories', {
+            url: '/',
+            views: {
+                'categories@': {
+                    controller: 'CategoriesListCtrl as CategoriesListCtrl',
+                    templateUrl: 'app/categories/categories.html'
+                }
+            }
+        })
+}])
+
+.controller('CategoriesListCtrl', ['CategoriesModel', function(CategoriesModel) {
+    var categoriesListCtrl = this;
+    CategoriesModel.getCategories()
+        .then(function(result) {
+            categoriesListCtrl.result = result;
+            console.log(result)
+        });
 }])
--- blog/bookmarks/bookmark-app/app/common/models/categories-model.js
+++ blog/bookmarks/bookmark-app/app/common/models/categories-model.js
@@ -1,2 +1,44 @@
-angular.module('app.models.categories', [
-])
+angular.module('app.models.categories', [])
+
+.service('CategoriesModel', ['$http', '$q', function($http, $q) {
+    var model = this,
+        URLS = {
+            FETCH: 'data/categories.json'
+        },
+        categories,
+        currentCategory;
+
+    function extract(result) {
+        return result.data;
+    };
+
+    function cacheCategories(result) {
+        categories = extract(result);
+        return categories;
+    };
+
+    model.getCategories = function() {
+        return (categories) ? $q.when(categories) : $http.get(URLS.FETCH).then(cacheCategories);
+    };
+
+    model.getCategoryByName = function(categoryName) {
+        var deferred = $q.defer();
+
+        function findCategory() {
+            return _.find(categories, function(c) {
+                return c.name == categoryName;
+            })
+        }
+
+        if (categories) {
+            deferred.resolve(findCategory());
+        } else {
+            model.getCategories()
+                .then(function(result) {
+                    deferred.resolve(findCategory())
+                })
+        }
+
+        return deferred.promise;
+    };
+}])
